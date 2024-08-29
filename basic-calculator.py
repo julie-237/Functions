02e16993c10fd4aaa5b427b507827b091faf318a
unknown <blandinedjoumsie@gmail.com>
2024-08-29 17:33:26 +0200

    added functions

--- basic-calculator.py
+++ basic-calculator.py
@@ -1,3 +1,25 @@
+def addition():
+    result = number_1 + number_2
+    return result
+
+def subtraction():
+    result = number_1 - number_2
+    return result
+
+def multiplication():
+    result = number_1 * number_2
+    return result
+
+def division():
+    result = number_1 / number_2
+    return result
+
+
+
+
+
+
+
 print("This calculator can perform: addition, subtraction, multiplication and division")
 number_1 = input("enter a number")
 number_2 = input("enter an other number")
